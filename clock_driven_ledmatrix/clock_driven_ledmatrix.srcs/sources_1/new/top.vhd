87157bd9c856b336c92c5d5f2dd83393c0b89262
quintenvg1 <38690465+quintenvg1@users.noreply.github.com>
2022-01-19 14:20:20 +0100

    kinda have an idea what i'm doing now
    as i said i thnik i know what i'm doing now

--- clock_driven_ledmatrix/clock_driven_ledmatrix.srcs/sources_1/new/top.vhd
+++ clock_driven_ledmatrix/clock_driven_ledmatrix.srcs/sources_1/new/top.vhd
@@ -31,13 +31,36 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity top is
---  Port ( );
+
+--declare the driver
+
+
+
+entity top is --all the inputs, outputs are driven from underlying components
+  Port (
+    clk : in STD_LOGIC;
+    Xrow : STD_LOGIC_VECTOR(7 downto 0);
+    Yrow : STD_LOGIC_VECTOR(7 downto 0)
+   );
 end top;
+
 
 architecture Behavioral of top is
 
+--component definitions
+component matrix_driver is
+    Port ( 
+           clk : in STD_LOGIC; --connects 100MHz to the display driver
+           xrow : in STD_LOGIC_VECTOR (7 downto 0);
+           yrow : in STD_LOGIC_VECTOR (7 downto 0)
+           --x : out STD_LOGIC_VECTOR (7 downto 0); not needed in top driven from driver
+           --y : out STD_LOGIC_VECTOR (7 downto 0) not needed in top driven from driver
+           );
+end component;
+
+
 begin
 
+driver:matrix_driver port map(clk => clk, Xrow => xrow, Yrow => yrow); --connects clock?
 
 end Behavioral;
